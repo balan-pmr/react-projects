fffba937564ac941e0610be690bc7ed0600c395f
Pedro Balan Martinez Rosales <balan.pmr@outlook.com>
2020-07-09 13:55:18 -0500

    Enviroment configurations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PostList from './PostList/PostList'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,21 +9,10 @@ function App() {
       <br/>
       <PostList/>
       <br/>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+      <div>
+          env value: {process.env.REACT_APP_NOT_SECRET_CODE} 
+      </div>
  
 
     </div>
